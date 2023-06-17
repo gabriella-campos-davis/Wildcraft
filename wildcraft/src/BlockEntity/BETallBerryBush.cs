65b3ebd299479702b3bac36f964fb8f8facb7e61
Gabby <gcamposdavis@gmail.com>
2023-06-17 14:12:49 -0700

    fixed berry bush stuff

--- wildcraft/src/BlockEntity/BETallBerryBush.cs
+++ wildcraft/src/BlockEntity/BETallBerryBush.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Vintagestory.API;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
 using Vintagestory.API.Config;
 using Vintagestory.API.Datastructures;
 using Vintagestory.API.MathTools;
@@ -15,17 +16,29 @@ using Vintagestory.GameContent;
 
 namespace wildcraft 
 {
-    public class BETallBerryBush : BEWildcraftBerryBush
-    {
+    public class BETallBerryBush : BlockEntity, IAnimalFoodSource
+        {
+        static Random rand = new Random();
 
         double lastCheckAtTotalDays = 0;
         double transitionHoursLeft = -1;
-
         double? totalDaysForNextStageOld = null; // old v1.13 data format, here for backwards compatibility
 
         RoomRegistry roomreg;
-        new public int roomness;
-        
+        public int roomness;
+
+        public bool Pruned;
+        public double LastPrunedTotalDays;
+
+        float resetBelowTemperature = 0;
+        float resetAboveTemperature = 0;
+        float stopBelowTemperature = 0;
+        float stopAboveTemperature = 0;
+        float revertBlockBelowTemperature = 0;
+        float revertBlockAboveTemperature = 0;
+
+        float growthRateMul = 1f;
+
         public BETallBerryBush() : base()
         {
 
@@ -34,6 +47,8 @@ namespace wildcraft
         public override void Initialize(ICoreAPI api)
         {
             base.Initialize(api);
+
+            growthRateMul = (float)Api.World.Config.GetDecimal("cropGrowthRateMul", growthRateMul);
 
             if (api is ICoreServerAPI)
             {
@@ -55,10 +70,19 @@ namespace wildcraft
                 {
                     transitionHoursLeft = ((double)totalDaysForNextStageOld - Api.World.Calendar.TotalDays) * Api.World.Calendar.HoursPerDay;
                 }
+
+                
             }
         }
 
-        public void CheckGrow(float dt)
+        internal void Prune()
+        {
+            Pruned = true;
+            LastPrunedTotalDays = Api.World.Calendar.TotalDays;
+            MarkDirty(true);
+        }
+
+        private void CheckGrow(float dt)
         {
             if (!(Api as ICoreServerAPI).World.IsFullyLoadedChunk(Pos)) return;
 
@@ -87,16 +111,8 @@ namespace wildcraft
 
             bool changed = false;
             float oneHour = 1f / Api.World.Calendar.HoursPerDay;
-            float resetBelowTemperature = 0, resetAboveTemperature = 0, stopBelowTemperature = 0, stopAboveTemperature = 0, revertBlockBelowTemperature = 0, revertBlockAboveTemperature = 0;
             if (daysToCheck > oneHour)
             {
-                resetBelowTemperature = Block.Attributes["resetBelowTemperature"].AsFloat(-999);
-                resetAboveTemperature = Block.Attributes["resetAboveTemperature"].AsFloat(999);
-                stopBelowTemperature = Block.Attributes["stopBelowTemperature"].AsFloat(-999);
-                stopAboveTemperature = Block.Attributes["stopAboveTemperature"].AsFloat(999);
-                revertBlockBelowTemperature = Block.Attributes["revertBlockBelowTemperature"].AsFloat(-999);
-                revertBlockAboveTemperature = Block.Attributes["revertBlockAboveTemperature"].AsFloat(999);
-
                 if (Api.World.BlockAccessor.GetRainMapHeightAt(Pos) > Pos.Y) // Fast pre-check
                 {
                     Room room = roomreg?.GetRoomForPosition(Pos);
@@ -164,18 +180,51 @@ namespace wildcraft
             if (changed) MarkDirty(false);
         }
 
-        public bool DoGrow()
+        public override void OnExchanged(Block block)
         {
-            if (Api.World.BlockAccessor.GetBlock(Pos.X, Pos.Y + 1, Pos.Z).BlockMaterial == 0 && 
-                !(Api.World.BlockAccessor.GetBlock(Pos.X, Pos.Y - 1, Pos.Z) is WildcraftBerryBush))
-            {
-                Block clipping = Api.World.BlockAccessor.GetBlock(AssetLocation.Create("wildcraft:growth-" + this.Block.Variant["type"] + "-alive"));
-                if(clipping is null) {
-                    throw new ArgumentNullException(nameof(clipping), "BETallBerryBush growth is Null. Exiting");
-                }
-                Api.World.BlockAccessor.SetBlock(clipping.BlockId, Pos.AddCopy(0, 1, 0));
-            }
+            base.OnExchanged(block);
+            if (Api?.Side == EnumAppSide.Server) UpdateTransitionsFromBlock();
+        }
 
+        public override void CreateBehaviors(Block block, IWorldAccessor worldForResolve)
+        {
+            base.CreateBehaviors(block, worldForResolve);
+            if (worldForResolve.Side == EnumAppSide.Server) UpdateTransitionsFromBlock();
+        }
+
+        protected void UpdateTransitionsFromBlock()
+        {
+            // In case we have a Block which is not a BerryBush block (why does this happen?)
+            if (Block?.Attributes == null)
+            {
+                resetBelowTemperature = stopBelowTemperature = revertBlockBelowTemperature = -999;
+                resetAboveTemperature = stopAboveTemperature = revertBlockAboveTemperature = 999;
+                return;
+            }
+            // These Attributes lookups are costly because Newtonsoft JSON lib ~~sucks~~ uses a weird approximation to a Dictionary in JToken.TryGetValue() but it can ignore case
+            resetBelowTemperature = Block.Attributes["resetBelowTemperature"].AsFloat(-999);
+            resetAboveTemperature = Block.Attributes["resetAboveTemperature"].AsFloat(999);
+            stopBelowTemperature = Block.Attributes["stopBelowTemperature"].AsFloat(-999);
+            stopAboveTemperature = Block.Attributes["stopAboveTemperature"].AsFloat(999);
+            revertBlockBelowTemperature = Block.Attributes["revertBlockBelowTemperature"].AsFloat(-999);
+            revertBlockAboveTemperature = Block.Attributes["revertBlockAboveTemperature"].AsFloat(999);
+        }
+
+        public double GetHoursForNextStage()
+        {
+            if (IsRipe()) return 4 * (5 + rand.NextDouble()) * 1.6 * Api.World.Calendar.HoursPerDay;
+
+            return (5 + rand.NextDouble()) * 1.6 * Api.World.Calendar.HoursPerDay / growthRateMul;
+        }
+
+        public bool IsRipe()
+        {
+            Block block = Api.World.BlockAccessor.GetBlock(Pos);
+            return block.LastCodePart() == "ripe";
+        }
+
+        bool DoGrow()
+        {
             if (Api.World.Calendar.TotalDays - LastPrunedTotalDays > Api.World.Calendar.DaysPerYear)
             {
                 Pruned = false;
@@ -185,9 +234,8 @@ namespace wildcraft
             string nowCodePart = block.LastCodePart();
             string nextCodePart = (nowCodePart == "empty") ? "flowering" : ((nowCodePart == "flowering") ? "ripe" : "empty");
 
-            if(block is null) return false;
-            AssetLocation loc = block.CodeWithPart(nextCodePart, 1);
 
+            AssetLocation loc = block.CodeWithParts(nextCodePart);
             if (!loc.Valid)
             {
                 Api.World.BlockAccessor.RemoveBlockEntity(Pos);
@@ -195,8 +243,14 @@ namespace wildcraft
             }
 
             Block nextBlock = Api.World.GetBlock(loc);
-
             if (nextBlock?.Code == null) return false;
+
+            if (nextCodePart == "ripe" && (Api.World.BlockAccessor.GetBlock(Pos.X, Pos.Y - 1, Pos.Z) is not WildcraftBerryBush) && 
+                Api.World.BlockAccessor.GetBlock(Pos.X, Pos.Y + 1, Pos.Z).BlockMaterial == 0) 
+            {
+                Block clipping = Api.World.BlockAccessor.GetBlock(AssetLocation.Create("wildcraft:growth-" + this.Block.Variant["type"] + "-alive"));
+                if (clipping is not null) Api.World.BlockAccessor.SetBlock(clipping.BlockId, Pos.AddCopy(0, 1, 0));
+            }
 
             Api.World.BlockAccessor.ExchangeBlock(nextBlock.BlockId, Pos);
 
@@ -204,5 +258,145 @@ namespace wildcraft
             return true;
         }
 
+
+        
+
+        public override void FromTreeAttributes(ITreeAttribute tree, IWorldAccessor worldForResolving)
+        {
+            base.FromTreeAttributes(tree, worldForResolving);
+
+            transitionHoursLeft = tree.GetDouble("transitionHoursLeft");
+
+            if (tree.HasAttribute("totalDaysForNextStage")) // Pre 1.13 format
+            {
+                totalDaysForNextStageOld = tree.GetDouble("totalDaysForNextStage");
+            }
+
+            lastCheckAtTotalDays = tree.GetDouble("lastCheckAtTotalDays");
+
+            roomness = tree.GetInt("roomness");
+            Pruned = tree.GetBool("pruned");
+            LastPrunedTotalDays = tree.GetDecimal("lastPrunedTotalDays");
+        }
+
+        public override void ToTreeAttributes(ITreeAttribute tree)
+        {
+            base.ToTreeAttributes(tree);
+
+            tree.SetDouble("transitionHoursLeft", transitionHoursLeft);
+            tree.SetDouble("lastCheckAtTotalDays", lastCheckAtTotalDays);
+            tree.SetBool("pruned", Pruned);
+            tree.SetInt("roomness", roomness);
+            tree.SetDouble("lastPrunedTotalDays", LastPrunedTotalDays);
+        }
+
+        public override void GetBlockInfo(IPlayer forPlayer, StringBuilder sb)
+        {
+            Block block = Api.World.BlockAccessor.GetBlock(Pos);
+            double daysleft = transitionHoursLeft / Api.World.Calendar.HoursPerDay;
+
+            /*if (forPlayer.WorldData.CurrentGameMode == EnumGameMode.Creative)
+            {
+                return "" + daysleft;
+            }*/
+
+            if (block.LastCodePart() == "ripe")
+            {
+                return;
+            }
+
+            string code = (block.LastCodePart() == "empty") ? "flowering" : "ripen";
+
+            if (daysleft < 1)
+            {
+                sb.AppendLine(Lang.Get("berrybush-"+ code + "-1day"));
+            }
+            else
+            {
+                sb.AppendLine(Lang.Get("berrybush-" + code + "-xdays", (int)daysleft));
+            }
+
+            if (roomness > 0)
+            {
+                sb.AppendLine(Lang.Get("greenhousetempbonus"));
+            }
+        }
+
+
+
+        #region IAnimalFoodSource impl
+        public bool IsSuitableFor(Entity entity, string[] diet)
+        {
+            if (diet == null) return false;
+
+            if (!diet.Contains("Berry")) return false;
+
+            if (!IsRipe()) return false;
+
+            return true;
+        }
+
+        public float ConsumeOnePortion()
+        {
+            AssetLocation loc = Block.CodeWithParts("empty");
+            if (!loc.Valid)
+            {
+                Api.World.BlockAccessor.RemoveBlockEntity(Pos);
+                return 0f;
+            }
+
+            Block nextBlock = Api.World.GetBlock(loc);
+            if (nextBlock?.Code == null) return 0f;
+
+            var bbh = Block.GetBehavior<BlockBehaviorHarvestable>();
+            if (bbh?.harvestedStack != null)
+            {
+                ItemStack dropStack = bbh.harvestedStack.GetNextItemStack();
+                Api.World.PlaySoundAt(bbh.harvestingSound, Pos.X + 0.5, Pos.Y + 0.5, Pos.Z + 0.5);
+                Api.World.SpawnItemEntity(dropStack, Pos.ToVec3d().Add(0.5, 0.5, 0.5));
+            }
+
+
+            Api.World.BlockAccessor.ExchangeBlock(nextBlock.BlockId, Pos);
+            MarkDirty(true);
+
+            return 0.1f;
+        }
+
+        public Vec3d Position => base.Pos.ToVec3d().Add(0.5, 0.5, 0.5);
+        public string Type => "food";
+        #endregion
+
+
+        public override void OnBlockRemoved()
+        {
+            base.OnBlockRemoved();
+
+            if (Api.Side == EnumAppSide.Server)
+            {
+                Api.ModLoader.GetModSystem<POIRegistry>().RemovePOI(this);
+            }
+        }
+
+        public override void OnBlockUnloaded()
+        {
+            base.OnBlockUnloaded();
+
+            if (Api?.Side == EnumAppSide.Server)
+            {
+                Api.ModLoader.GetModSystem<POIRegistry>().RemovePOI(this);
+            }
+        }
+
+        public override bool OnTesselation(ITerrainMeshPool mesher, ITesselatorAPI tessThreadTesselator)
+        {
+            if (Pruned)
+            {
+                mesher.AddMeshData((Block as WildcraftBerryBush).GetPrunedMesh(Pos));
+                return true;
+            }
+
+            return base.OnTesselation(mesher, tessThreadTesselator);
+        }
     }
 }
